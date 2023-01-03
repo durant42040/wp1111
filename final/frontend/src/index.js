6fb51672125755371a564da8f8bd1f7c7080e964
b11901040 <113010586+b11901040@users.noreply.github.com>
2023-01-03 23:44:18 +0800

    Add files via upload

---
+++ final/frontend/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {ChatProvider} from './hooks/useData'
+import { Auth0Provider } from "@auth0/auth0-react";
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <ApolloProvider client={client}>
+          <Auth0Provider domain='dev-470m0yzjwr08gok5.us.auth0.com' clientId='Hsip7rajodFYjCgS79g1tSXwuUZSfMPA' redirectUri={window.location.origin}>
+              <ChatProvider>
+                  <App/>
+              </ChatProvider>
+          </Auth0Provider>
+      </ApolloProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
